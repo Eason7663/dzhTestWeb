ae1e9bd89da3efe63bdf1674c5a99c126ca36011
Eason.ZHANG <zhyj663@163.com>
2018-04-24 23:38:42 +0800

    多线程练习，生产者/消费者，队列

--- appaction/jmeterThread.py
+++ appaction/jmeterThread.py
@@ -11,36 +11,70 @@
                    2018/4/24:
 -------------------------------------------------
 """
-from appaction.jmeterServer import JmeterServer
+# from appaction.jmeterServer import JmeterServer
 import threading
 from queue import Queue
-import time,random
+import time,random,math
 
-class JmeterThread(threading.Thread):
-    taskQueue = Queue()
-    def __init__(self,i):
-        threading.Thread.__init__(self)
-        # self.jmeterSvr = jmeterSvr
-        self.i = i
+taskQueue = Queue()
+# class JmeterThread(threading.Thread):
+#
+#     def __init__(self,i):
+#         threading.Thread.__init__(self)
+#         # self.jmeterSvr = jmeterSvr
+#         self.i = i
+#
+#     def run(self):
+#         # self.jmeterSvr.run_cmd()
+#         ii = random.randint(0,self.i)
+#         time.sleep(ii)
+#
+#         s = "sleep " + ii + "s"
+#         self.taskQueue.put(s)
 
-    def run(self):
-        # self.jmeterSvr.run_cmd()
-        ii = random.randint(0,self.i)
-        time.sleep(ii)
+class odd():
+    def __init__(self,num):
+        self.num = num
 
-        s = "sleep " + ii + "s"
-        self.taskQueue.put(s)
+    def __str__(self):
+        return "I({}) am a odd".format(str(self.num))
+
+
+class even():
+    def __init__(self, num):
+        self.num = num
+
+    def __str__(self):
+        return "I({}) am a even".format(str(self.num))
+
+def worker(name):
+
+    ii = random.randint(0, 20)
+    # time.sleep(ii)
+
+        # s = name + " sleep " + str(ii) + "s"
+    print("In thread: " + threading.current_thread().getName())
+    if (ii%2)==0:
+        print(ii)
+        # even(ii)
+        taskQueue.put(even(ii))
+    else:
+        # odd(ii)
+        taskQueue.put(odd(ii))
 
 
 if __name__=="__main__":
-
     for i in range(10):
-
-
-    JmeterThread.taskQueue.put(1)
-    JmeterThread.taskQueue.put(2)
-    JmeterThread.taskQueue.put(4)
-    JmeterThread.taskQueue.put(2)
-    print('LILO队列', JmeterThread.taskQueue)  # 查看队列中的所有元素
-    print(JmeterThread.taskQueue.get())  # 返回并删除队列头部元素
-    print(JmeterThread.taskQueue.queue)
+        t = threading.Thread(target=worker,args=("Thread{}".format(i),),name="Thread"+str(i))
+        t.start()
+    j = 5
+    while True:
+        time.sleep(3)
+        if j <=0 :
+            break
+        j -= 1
+        tt = taskQueue.get()
+        if isinstance(tt,odd):
+            print("***odd:"+ tt.__str__())
+        else:
+            print("***even:"+ tt.__str__())
