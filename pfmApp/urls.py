8bd67dceee79d9b718a7ca6fc06798998d17a587
Eason.ZHANG <zhyj663@163.com>
2018-05-02 21:43:18 +0800

    1.优化工程管理，组件管理，用例管理界面 2.增加左侧导航菜单

--- pfmApp/urls.py
+++ pfmApp/urls.py
@@ -5,13 +5,15 @@
 @file: urls.py
 @time: 2018/04/29
 """
-# from django.conf.urls import url,include
-# from . import views
-# from rest_framework.routers import DefaultRouter,SimpleRouter
-#
-# router = SimpleRouter()
-# # router.register(r'jmx_analyze/', views.testTable)
-#
-# urlpatterns = [
-#     url(r'^jmx/analyze/$', views.testTable,name="jmx_analyze"),
-# ]
+
+from django.conf.urls import url,include
+from . import views
+from rest_framework.routers import DefaultRouter,SimpleRouter
+
+router = SimpleRouter()
+# router.register(r'jmx_analyze/', views.testTable)
+
+urlpatterns = [
+    url(r'^jmx/analyze/$', views.testTable,name="jmx_analyze"),
+    url(r'pfmCase/home$',views.testTable)
+]
